c7c96a952eec4a06a0fd1e62e1c2fc6733951ef3
Daniela Passos <moitidani@gmail.com>
2022-08-29 10:08:17 +0300

    Updated header
    Title broken into h1 & h3

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import Search from "./Search";
 function App() {
   return (
     <div className="App">
-      <h1>Final lesson week 4</h1>
+      <h1>Weather app</h1>
+      <h3>Final lesson week 4</h3>
       <Search />
       <small><a href="https://github.com/danielapassos/weather-react1">Open Source code</a> by Daniela Passos</small>
     </div>
